0270414e567f63098199550f76ecb04b714f61e2
Friggo <filip.macek@hotmail.cz>
2025-08-03 09:47:15 +0200

    chore: add cdn to remote Patterns in next config

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
   output: "standalone",
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...[NEXT_PUBLIC_SERVER_URL, 'https://cdn.nakashi.cz/' ].map((item) => {
         const url = new URL(item);
 
         return {
